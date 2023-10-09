acf1731389c111ce090c33e0a218c09e25a2bf0d
Louis Law <louiscklaw@users.noreply.github.com>
2023-10-09 15:02:19 +0800

    update carousell-monitor-playlist,

--- carousell-search-scrape/puppeteer-carousell-helloworld.js
+++ carousell-search-scrape/puppeteer-carousell-helloworld.js
@@ -2,4 +2,18 @@ const express = require('express');
 const puppeteer = require('puppeteer-extra');
 const fs = require('fs');
 
+(async () => {
+  var browser;
+  var output = {
+    state: 'init',
+    debug: {},
+    error: {},
+    scrape_result: [],
+    news_link: [],
+    feed_discord: { last_update: new Date().toISOString(), links: [] },
+  };
+
+  browser && browser.close();
+})();
+
 console.log('puppeteer-carousell-helloworld');
