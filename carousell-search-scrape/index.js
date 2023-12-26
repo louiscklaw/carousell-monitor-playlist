3e9ed41ef6bf7362fc084d7e7c7af5653ab5e419
Louis Law <louiscklaw@users.noreply.github.com>
2023-12-26 14:23:38 +0800

    update carousell-search-scrape,

--- carousell-search-scrape/index.js
+++ carousell-search-scrape/index.js
@@ -91,6 +91,14 @@ puppeteer.use(StealthPlugin());
         var search_result = await scrapeCarousellSearchResult(page);
         var search_result_json = JSON.parse(search_result);
         output.result['search_using_tableau'] = search_result_json;
+
+
+        await page.goto('https://www.carousell.com.hk/search/wordpress', { waitUntil: 'networkidle2' });
+        await page.setViewport({ width: 1920, height: 1080 * 5 });
+        await page.screenshot({ path: './screens/300-wordpress-helloworld.png' });
+        var search_result = await scrapeCarousellSearchResult(page);
+        var search_result_json = JSON.parse(search_result);
+        output.result['search_using_wordpress'] = search_result_json;
     } catch (error) {
         console.log(error);
         output = { ...output, state: 'error puppeteer-carousell-helloworld.js', error };
