55ad1f6846630327a6a724a97b602c7e50af8523
Louis Law <louiscklaw@users.noreply.github.com>
2024-02-02 01:37:20 +0800

    update,

--- monitors/my_post/index.js
+++ monitors/my_post/index.js
@@ -1,47 +1,56 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 
-var search_result = fs.readFileSync('../carousell-search-scrape/results/carousell_search_result.json', {
+var search_result = fs.readFileSync('../_tmp/results/carousell_search_result.json', {
   encoding: 'utf-8',
 });
 var search_result_json = JSON.parse(search_result);
+
 var { result } = search_result_json;
-var { search_using_vba,
+var {
+  search_using_vba,
   search_using_coding,
   search_using_programming,
   search_using_python,
   search_using_javascript,
   search_using_tableau,
-  search_using_wordpress } = result;
+  search_using_wordpress,
+} = result;
 
-function testMain(list_under_test) {
-  var vba_post_check = 'FAILED';
+function testMain(list_under_test, kw) {
+  try {
+    var vba_post_check = 'FAILED';
 
-  for (var i = 0; i < 24; i++) {
-    // console.log(search_using_vba[i].seller_name);
-    if (list_under_test[i].seller_name.search(/cklaw/) > -1) {
-      vba_post_check = 'OK';
-      break;
+    for (var i = 0; i < 24; i++) {
+      // console.log(search_using_vba[i].seller_name);
+      if (!list_under_test) throw new Error(`${kw} list is undefined`);
+      if (list_under_test[i].seller_name.search(/cklaw/) > -1) {
+        vba_post_check = 'OK';
+        break;
+      }
+
+      if (list_under_test[i].seller_name.search(/ckwong/) > -1) {
+        vba_post_check = 'OK';
+        break;
+      }
     }
-
-    if (list_under_test[i].seller_name.search(/ckwong/) > -1) {
-      vba_post_check = 'OK';
-      break;
-    }
+    return vba_post_check;
+  } catch (error) {
+    console.log(list_under_test);
+    console.log(error);
+    throw new Error(error);
   }
-  return vba_post_check;
 }
 
 (() => {
   var output = { state: 'init', debug: {}, error: {}, result: { last_update: new Date().toISOString() } };
-  var vba_post_check = testMain(search_using_vba);
-  var coding_post_check = testMain(search_using_coding);
-  var coding_post_check = testMain(search_using_coding);
-  var programming_post_check = testMain(search_using_programming);
-  var javascript_post_check = testMain(search_using_javascript);
-  var python_post_check = testMain(search_using_python);
-  var tableau_post_check = testMain(search_using_tableau);
-  var wordpress_post_check = testMain(search_using_wordpress);
+  var vba_post_check = testMain(search_using_vba, 'vba');
+  var coding_post_check = testMain(search_using_coding, 'coding');
+  var programming_post_check = testMain(search_using_programming, 'programming');
+  var javascript_post_check = testMain(search_using_javascript, 'javascript');
+  var python_post_check = testMain(search_using_python, 'python');
+  var tableau_post_check = testMain(search_using_tableau, 'tableau');
+  var wordpress_post_check = testMain(search_using_wordpress, 'wordpress');
 
   try {
     output.result['vba_post_check'] = vba_post_check;
@@ -55,4 +64,5 @@ function testMain(list_under_test) {
     output = { ...output };
   }
   fs.writeFileSync('./results/post_test_result.json', JSON.stringify(output, null, 2), { encoding: 'utf-8' });
+  fs.writeFileSync('../_tmp/results/post_test_result.json', JSON.stringify(output, null, 2), { encoding: 'utf-8' });
 })();
